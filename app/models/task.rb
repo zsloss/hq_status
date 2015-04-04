313012bc6915af75c4fd28905aa9a57ff0b5bdf5
Zachary Sloss <zachary.sloss@gmail.com>
2015-04-05 00:00:58 +0800

    Add validation and refine form

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,5 @@
 class Task < ActiveRecord::Base
+	validates :product, presence: true
+	validates :doc_type, presence: true, inclusion: { in: %w(Datasheet Manual QIG), message: "Document Type must be a Datasheet, Manual, or QIG" }
+	validates :status, presence: true, inclusion: { in: %w(Queued In\ Progress Pending), message: "must be Queued, In Progress, or Pending" }
 end
