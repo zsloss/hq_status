02044aaf0f7b5634e4285f2c9c21231d8b248b9b
Zachary Sloss <zachary.sloss@gmail.com>
2015-04-05 11:51:29 +0000

    Removed unneeded task routes, basic boostrap, task index view refinements

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -30,9 +30,9 @@ class TasksController < ApplicationController
     respond_to do |format|
       if @task.save
         format.html { redirect_to @task, notice: 'Task was successfully created.' }
-        format.json { render :show, status: :created, location: @task }
+        format.json { render :index, status: :created, location: @task }
       else
-        format.html { render :new }
+        format.html { render :index }
         format.json { render json: @task.errors, status: :unprocessable_entity }
       end
     end
@@ -44,7 +44,7 @@ class TasksController < ApplicationController
     respond_to do |format|
       if @task.update(task_params)
         format.html { redirect_to @task, notice: 'Task was successfully updated.' }
-        format.json { render :show, status: :ok, location: @task }
+        format.json { render :index, status: :ok, location: @task }
       else
         format.html { render :edit }
         format.json { render json: @task.errors, status: :unprocessable_entity }
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resources :tasks  
+  resources :tasks, only: [:index, :edit, :create, :update, :destroy]  
 end
