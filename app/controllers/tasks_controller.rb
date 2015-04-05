92e7fc870ee8a4177ce723b827936a7ee5600225
Zachary Sloss <zachary.sloss@gmail.com>
2015-04-05 13:22:47 +0000

    Fixed task controller bugs with validations and redirects

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,16 +8,6 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
-  # GET /tasks/1
-  # GET /tasks/1.json
-  def show
-  end
-
-  # GET /tasks/new
-  def new
-    @task = Task.new
-  end
-
   # GET /tasks/1/edit
   def edit
   end
@@ -26,10 +16,10 @@ class TasksController < ApplicationController
   # POST /tasks.json
   def create
     @task = Task.new(task_params)
-
+    @tasks = Task.all
     respond_to do |format|
       if @task.save
-        format.html { redirect_to @task, notice: 'Task was successfully created.' }
+        format.html { redirect_to tasks_path, notice: 'Task was successfully created.' }
         format.json { render :index, status: :created, location: @task }
       else
         format.html { render :index }
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,11 @@
 class Task < ActiveRecord::Base
 	validates :product, presence: true
-	validates :doc_type, presence: true, inclusion: { in: %w(Datasheet Manual QIG), message: "Document Type must be a Datasheet, Manual, or QIG" }
-	validates :status, presence: true, inclusion: { in: %w(Queued In\ Progress Pending), message: "must be Queued, In Progress, or Pending" }
+	validates :doc_type, inclusion: { in: %w(Datasheet Manual QIG), message: "Document Type must be a Datasheet, Manual, or QIG" }
+	validates :status, inclusion: { in: %w(Queued In\ Progress Pending), message: "must be Queued, In Progress, or Pending" }
+	before_validation :default_values
+
+	private
+	def default_values
+		self.status ||= "Queued"
+	end
 end
