4c6fa17e856c5a26dd2dc0b83a77a1037669a1dd
Zachary Sloss <zachary.sloss@gmail.com>
2015-04-12 06:08:51 +0000

    Add working signup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,19 @@
 class UsersController < ApplicationController
   def new
+  	@user = User.new
+  end
+
+  def create
+    @user = User.new(user_params) 
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to users_path, notice: 'User was successfully added.' }
+        format.json { render :index, status: :created, location: @user }
+      else
+        format.html { render :index }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def index
@@ -9,5 +23,10 @@ class UsersController < ApplicationController
   end
 
   def delete
+  end
+
+  private
+  def user_params
+  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 	resources :tasks, only: [:index, :edit, :create, :update, :destroy]
 
-	resources :users, only: [:index, :edit, :create, :update, :destroy] do
+	resources :users, only: [:new, :index, :edit, :create, :update, :destroy] do
 		resources :tasks, only: [:index, :edit, :create, :update, :destroy]
 	end
 end
