6514dda673f6083b16ec4bc36c3ea533dca786e8
Zachary Sloss <zachary.sloss@gmail.com>
2015-04-12 04:42:14 +0000

    Add basic authentication

--- Gemfile
+++ Gemfile
@@ -4,6 +4,8 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.1'
 
+gem 'bcrypt'
+
 gem 'bootstrap-sass'
 
 # Use sqlite3 as the database for Active Record
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ActiveRecord::Base
+	before_save { self.email = email.downcase }
+
 	has_many :tasks, foreign_key: 'writer_id'
 	has_many :peer_reviews, class_name: 'Task', foreign_key: 'reviewer_id'
 
 	validates :name, presence: true
 	validates :email, presence: true
+
+	has_secure_password
 
 	# Returns a hash in the form { name => id }
 	def self.get_options_for_select
