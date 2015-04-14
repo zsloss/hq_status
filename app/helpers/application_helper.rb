a26b8ffa735e59393ec1e309af9fc892e5adae60
Zachary Sloss <zachary.sloss@gmail.com>
2015-04-14 18:35:29 +0000

    Refactor sort link functions and add to tasks admin page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,23 @@
 module ApplicationHelper
+
+	def sort_link(body, attribute)
+		link_to body, { action: action_name }.merge(sort_params attribute)
+	end
+
+	def sort_tasks(tasks, default="flow")
+		params[:sort_by] ||= default
+		criterion = Proc.new { |t| t.send params[:sort_by] } # attribute to sort by stored in params
+		sorted_tasks = tasks.select(&criterion).sort_by(&criterion)
+		nil_attr_tasks = tasks.reject(&criterion)
+		tasks = sorted_tasks + nil_attr_tasks # nil attributes at the end
+		tasks.reverse! if params[:order_by] == 'desc'
+		return tasks
+	end
+
+	private
+
 	def sort_params(attribute)
 		{ sort_by: attribute, order_by: !params.key?(:order_by) && params[:sort_by] == attribute ? 'desc' : nil }
-	end
+	end	
+	
 end
